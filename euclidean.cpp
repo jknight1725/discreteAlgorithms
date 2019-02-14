1d8ffad1828ba03f36261208dc1ff8170db7de56
James Knight <james.p.knight2@gmail.com>
2019-02-14 11:00:26 -0700

    Add files via upload

---
+++ euclidean.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+uint32_t euclidean(uint32_t a, uint32_t b) {
+	if (a%b == 0) {
+		return b;
+	}
+	else {
+		return euclidean(b,a%b);
+	}
+}
+
+int main() {
+	uint32_t a, b = 1;
+	std::cout << "This program attempts to find the GCD of two numbers";
+	std::cout << "\nEnter the first number\n";
+	std::cin >> a;
+	std::cout << "Enter the second number\n";
+	std::cin >> b;
+	std::cout << "GCD(" << a << " " << b << ") = " << euclidean(a,b) <<'\n';
+	
+
+
+
+return 0;
+}
