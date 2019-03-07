e8095dfbd2bda1bf822a169d9a80d9d9340c38de
James Knight <james.p.knight2@gmail.com>
2019-03-07 15:27:23 -0700

    Update discreteGame.cpp

--- discreteGame.cpp
+++ discreteGame.cpp
@@ -12,16 +12,14 @@ int getDeck() {
 int getValue(int n) {
 	int value{0};
 	while(value == 0 || value > n) {
-		std::cout << "How many cards to remove?\n"
-			<<"Choose between 1-" << n << std::endl;
+		std::cout << "How many cards to remove?\n" << "Choose between 1-" << n << std::endl;
 		std::cin >> value; 
 	}
 	return value;
 }
 
 void printDecks(int deck1, int deck2) {
-	std::cout <<"Deck 1: "<< deck1 << "\nDeck 2: "
-                          << deck2 << std::endl;
+	std::cout <<"Deck 1: "<< deck1 << "\nDeck 2: " << deck2 << std::endl;
 }
 
 int main() {
@@ -36,11 +34,9 @@ int main() {
 		int sub = getValue(n);
 		deck == 1 ? deck1 -= sub : deck2 -= sub;
 		printDecks(deck1,deck2);
-		std::cout <<"CPU removes " << sub 
-					 << " cards from the other deck\n"; 
-		deck == 1 ? deck2 -= sub : deck1 -= sub; //same move dif deck
+		std::cout <<"CPU removes " << sub << " cards from the other deck\n"; 
+		deck == 1 ? deck2 -= sub : deck1 -= sub; //same move opposite deck
 		if(deck1<=0 && deck2<=0) break;
-		
 	}
 	std::cout << "CPU WINS\n";
 return 0;
