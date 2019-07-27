1a718b4d00ff7eb091dccf916a7e52c645b2a9bc
James Knight <james.p.knight2@gmail.com>
2019-07-27 06:39:36 -0600

    Create pascals_triangle.rb

---
+++ pascals_triangle.rb
@@ -0,0 +1,15 @@
+def pascal(row)
+  triangle = [1]
+  return triangle if row <= 1
+
+  0.upto(row - 1) do |k|
+    triangle.append(triangle[k] * (row - k) / (k + 1))
+  end
+  triangle
+end
+
+def pascal_triangle(rows)
+  1.upto(rows) { |x| puts pascal(x).to_s }
+end
+
+pascal_triangle(11)
